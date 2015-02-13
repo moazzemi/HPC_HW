11a713dde2a996322ceb9be74bb769c1e1263407
moazzemi <k.moazzemi@gmail.com>
2015-02-12 16:44:49 -0800

    modified dynamic render

--- mandelbrot.sh
+++ mandelbrot.sh
@@ -15,6 +15,6 @@ module load boost/1.57.0
 
 # Module load OpenMPI
 module load openmpi-1.8.3/gcc-4.8.3
-
+#change np for different run
 # Run the program 
 mpirun -x MXM_LOG_LEVEL=error -np 100  ./mandelbrot_mpi 1000 1000
